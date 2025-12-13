df6ecad4bc0bcca8e5ddd4295de429893ce2b871
DaniCab21 <danielcabiativa.dc@gmail.com>
2025-12-13 16:34:44 -0500

    Reinicio total del main

--- app/models/user.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from sqlmodel import Field, SQLModel
-
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    email: str = Field(unique=True, index=True)
-    hashed_password: str  # ¡OJO! Aquí guardamos el hash, nunca la pass real
-    is_active: bool = True
